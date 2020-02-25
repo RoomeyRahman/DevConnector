3c18d4cc1ecda72616176260c325e8619f9acb11
RoomeyRahman <roomeyrahman@gmail.com>
2020-02-25 19:16:22 +0600

    User existance check while registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const User = require("../../models/User");
+
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
@@ -17,7 +19,7 @@ router.post(
       "Please provide a password with at least 6 character"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     // console.log(req.body);
     const errors = validationResult(res);
 
@@ -25,7 +27,20 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User route");
+    const { name, email, password } = res.body;
+
+    try {
+      res.send("User route");
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
